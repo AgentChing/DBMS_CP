129831e1084afb54fadda3c10de0c72da24a195a
pushpak25w <sarafpushpak@gmail.com>
2022-03-13 13:11:37 +0530

    changed some maps to unordered_maps

--- DMSCP.cpp
+++ DMSCP.cpp
@@ -2,50 +2,50 @@
 #include "dmscp.h"
 using namespace std;
 
-class Table{
+class Table {
 public:
     string name;
     int attr_count;
-    map <string, string> attr_type;
+    unordered_map <string, string> attr_type;
     map<string, vector<string> > attr_value;
 
     void showtable()
     {
-        cout<<endl<<"Table name: "<<name<<endl<<"Attribute count: "<<attr_count<<endl;
-        for(auto x: attr_type)
+        cout << endl << "Table name: " << name << endl << "Attribute count: " << attr_count << endl;
+        for (auto x : attr_type)
         {
-            cout<<x.first<<" : "<<x.second<<"|  ";
+            cout << x.first << " : " << x.second << "|  ";
         }
-        cout<<endl;
+        cout << endl;
         int s;
-        for(auto x: attr_value)
+        for (auto x : attr_value)
         {
-            cout<<x.first<<"\t";
+            cout << x.first << "\t";
             s = x.second.size();
         }
-        for(int i=0;i<s;i++)
+        for (int i = 0; i < s; i++)
         {
-            cout<<endl;
-            for(auto x: attr_value)
+            cout << endl;
+            for (auto x : attr_value)
             {
-                cout<<x.second[i]<<"\t";
+                cout << x.second[i] << "\t";
             }
 
         }
-            cout<<endl;
+        cout << endl;
     }
 };
 
-class Database{
-unordered_map<string, vector<string> > metadata;
+class Database {
+    unordered_map<string, vector<string> > metadata;
 //string meta = "instructions.txt";
-map<string , Table> database;
+    unordered_map<string , Table> database;
 
-string metafile = "meta.txt";
-string datafile = "data.txt";
+    string metafile = "meta.txt";
+    string datafile = "data.txt";
 
 public:
-    Database(){
+    Database() {
         loadData();
     }
 
@@ -53,33 +53,33 @@ public:
     {
         metadata.clear();
         database.clear();
-     ifstream meta(metafile);
-     ifstream data(datafile);
-     string line;
-     while (getline (meta, line)) {
-              vector<string> v = tokenizefile(line);
-              stringstream num(v[1]);
-              int n =0;
-              num>>n;
-              Table t;
-              t.name = v[0];
-              t.attr_count = n;
-              v.erase(v.begin(),v.begin()+2);
-              for(int i=0;i<v.size();i+=2)
-              {
-                  t.attr_type.insert(pair<string,string>(v[i],v[i+1]));
-              }
-              for(int i=0;i<n;i++)
-              {
-                  string line2;
-                  getline (data, line2);
-                  vector<string> datavalues = tokenizefile(line2);
-                  datavalues.erase(datavalues.begin());
-                  t.attr_value.insert(pair<string, vector<string> >(v[i*2],datavalues));
-                  ///tablecolumns
-              }
-            database.insert(pair<string, Table>(t.name,t));
+        ifstream meta(metafile);
+        ifstream data(datafile);
+        string line;
+        while (getline (meta, line)) {
+            vector<string> v = tokenizefile(line);
+            stringstream num(v[1]);
+            int n = 0;
+            num >> n;
+            Table t;
+            t.name = v[0];
+            t.attr_count = n;
+            v.erase(v.begin(), v.begin() + 2);
+            for (int i = 0; i < v.size(); i += 2)
+            {
+                t.attr_type.insert(pair<string, string>(v[i], v[i + 1]));
             }
+            for (int i = 0; i < n; i++)
+            {
+                string line2;
+                getline (data, line2);
+                vector<string> datavalues = tokenizefile(line2);
+                datavalues.erase(datavalues.begin());
+                t.attr_value.insert(pair<string, vector<string> >(v[i * 2], datavalues));
+                ///tablecolumns
+            }
+            database.insert(pair<string, Table>(t.name, t));
+        }
         meta.close();
         data.close();
 
@@ -87,7 +87,7 @@ public:
 
     void showall()
     {
-        for(auto x: database)
+        for (auto x : database)
         {
             x.second.showtable();
         }
@@ -98,20 +98,20 @@ public:
         ofstream meta2;
         meta2.open("meta.txt");
         ofstream data2("data.txt");
-        for(auto x: database)
+        for (auto x : database)
         {
-            meta2<<x.first<<" "<<x.second.attr_count<<" ";
-            for(auto y: x.second.attr_type)
+            meta2 << x.first << " " << x.second.attr_count << " ";
+            for (auto y : x.second.attr_type)
             {
-                meta2<<y.first<<" "<<y.second<<" ";
-                data2<<y.first<<" ";
-                for(auto z: x.second.attr_value.find(y.first)->second)
+                meta2 << y.first << " " << y.second << " ";
+                data2 << y.first << " ";
+                for (auto z : x.second.attr_value.find(y.first)->second)
                 {
-                    data2<<z<<" ";
+                    data2 << z << " ";
                 }
-                data2<<endl;
+                data2 << endl;
             }
-            meta2<<endl;
+            meta2 << endl;
         }
         meta2.close();
         data2.close();
@@ -119,40 +119,40 @@ public:
 
     void addnew(vector<string> v)
     {
-        v.erase(v.begin(),v.begin()+2);
+        v.erase(v.begin(), v.begin() + 2);
         string name = v[0];
-        if(database.find(name) == database.end())
+        if (database.find(name) == database.end())
         {
             v.erase(v.begin());
-        cout<<name<<endl;
-        vector<vector< string > > attr = breakdowntokens(tokenizefile(v[0]));
-        Table t;
-        t.name = name;
-        t.attr_count = attr.size();
-          for(int i=0;i<attr.size();i++)
-          {
-              if(attr[i].size()==2)
-              {
-                  t.attr_type.insert(pair<string,string>(attr[i][0],attr[i][1]));
-                  vector<string> temp2;
-                  t.attr_value.insert(pair<string,vector<string> >(attr[i][0],temp2));
-              }
-              else if(attr[i].size()==4)
-              {
-                  t.attr_type.insert(pair<string,string>(attr[i][0],attr[i][1]));
-                  vector<string> temp2;
-                  t.attr_value.insert(pair<string,vector<string> >(attr[i][0],temp2));
-                  ///creat attributes and bound them with conditions;
-              }
-              else{
-                cout<<"ERROR: INCORRECT INPUT";
-                return;
-              }
-          }
-          database.insert(pair<string,Table>(name,t));
+            cout << name << endl;
+            vector<vector< string > > attr = breakdowntokens(tokenizefile(v[0]));
+            Table t;
+            t.name = name;
+            t.attr_count = attr.size();
+            for (int i = 0; i < attr.size(); i++)
+            {
+                if (attr[i].size() == 2)
+                {
+                    t.attr_type.insert(pair<string, string>(attr[i][0], attr[i][1]));
+                    vector<string> temp2;
+                    t.attr_value.insert(pair<string, vector<string> >(attr[i][0], temp2));
+                }
+                else if (attr[i].size() == 4)
+                {
+                    t.attr_type.insert(pair<string, string>(attr[i][0], attr[i][1]));
+                    vector<string> temp2;
+                    t.attr_value.insert(pair<string, vector<string> >(attr[i][0], temp2));
+                    ///creat attributes and bound them with conditions;
+                }
+                else {
+                    cout << "ERROR: INCORRECT INPUT";
+                    return;
+                }
+            }
+            database.insert(pair<string, Table>(name, t));
         }
-        else{
-            cout<<"COMMAND NOT EXECUTED: TABLE NAME ALREADY EXIST"<<endl;
+        else {
+            cout << "COMMAND NOT EXECUTED: TABLE NAME ALREADY EXIST" << endl;
         }
         storeData();
         loadData();
@@ -160,13 +160,13 @@ public:
 
     void droptable(string tbl_name)
     {
-        if(database.find(tbl_name) != database.end())
+        if (database.find(tbl_name) != database.end())
         {
             database.erase(tbl_name);
             storeData();
         }
-        else{
-            cout<<"COMMAND NOT EXECUTED: TABLE NOT FOUND"<<endl;
+        else {
+            cout << "COMMAND NOT EXECUTED: TABLE NOT FOUND" << endl;
         }
     }
 };
@@ -176,63 +176,63 @@ void driver(Database d)
 {
     string inst = "Instructions.txt";
     ifstream myfile(inst);
-     string line;
-     while (getline (myfile, line)) {
-              vector<string> v = tokenizefile(line);
-              for(int i=0;i<v.size();i++)
-              {
-                  ///cout<<v[i]<<endl;
-              }
-              if(absolutecompare(v[0],"CREATE"))
-              {
-                  cout<<"FOUND CREATE-------------\n";
-                  d.addnew(v);
-                  d.showall();
+    string line;
+    while (getline (myfile, line)) {
+        vector<string> v = tokenizefile(line);
+        for (int i = 0; i < v.size(); i++)
+        {
+            ///cout<<v[i]<<endl;
+        }
+        if (absolutecompare(v[0], "CREATE"))
+        {
+            cout << "FOUND CREATE-------------\n";
+            d.addnew(v);
+            d.showall();
 
-              }
-              else if(absolutecompare(v[0],"DROP"))
-              {
-                  cout<<"FOUND DROP-------------";
-                  d.droptable(v[2]);
-              }else if(absolutecompare(v[0],"SELECT"))
-              {
-                  cout<<"FOUND SELECT-------------";
-              }else if(absolutecompare(v[0],"INSERT"))
-              {
-                  cout<<"FOUND INSERT-------------";
+        }
+        else if (absolutecompare(v[0], "DROP"))
+        {
+            cout << "FOUND DROP-------------";
+            d.droptable(v[2]);
+        } else if (absolutecompare(v[0], "SELECT"))
+        {
+            cout << "FOUND SELECT-------------";
+        } else if (absolutecompare(v[0], "INSERT"))
+        {
+            cout << "FOUND INSERT-------------";
 
-              }else if(absolutecompare(v[0],"DELETE"))
-              {
-                  cout<<"FOUND DELETE-------------";
+        } else if (absolutecompare(v[0], "DELETE"))
+        {
+            cout << "FOUND DELETE-------------";
 
-              }else if(absolutecompare(v[0],"UPDATE"))
-              {
-                  cout<<"FOUND UPDATE-------------";
+        } else if (absolutecompare(v[0], "UPDATE"))
+        {
+            cout << "FOUND UPDATE-------------";
 
-              }else if(absolutecompare(v[0],"QUIT"))
-              {
-                  cout<<"FOUND QUIT-------------";
-                  d.storeData();
-                  d.showall();
-                  cout<<"\nDONE";
-                  myfile.close();
-                  return;
-
-              }
-              cout<<endl;
-            }
+        } else if (absolutecompare(v[0], "QUIT"))
+        {
+            cout << "FOUND QUIT-------------";
+            d.storeData();
+            d.showall();
+            cout << "\nDONE";
             myfile.close();
+            return;
+
+        }
+        cout << endl;
+    }
+    myfile.close();
 }
 
 
 int main()
 {
     Database d;
-    cout<<endl<<"//////////////////////////////////////////////////////////// "<<endl;
+    cout << endl << "//////////////////////////////////////////////////////////// " << endl;
     driver(d);
-    cout<<endl<<"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ "<<endl;
+    cout << endl << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ " << endl;
 
 
 
-    return 0;
-}
+         return 0;
+     }
